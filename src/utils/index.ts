c6f6f296b8cd1d5f1fb0082493ecf7417cceb334
StevenH <hongfanmeng@gmail.com>
2023-01-30 12:28:32 +0800

    feat: fetch content from strapi

---
+++ src/utils/index.ts
@@ -0,0 +1,23 @@
+import type {
+  ComponentGeneralArticle,
+  Maybe,
+  UploadFileEntityResponse,
+} from "../models/types";
+import type { CardBase } from "../components/Card.astro";
+
+const imageRespToUrl = (resp?: Maybe<UploadFileEntityResponse>) => {
+  if (resp) return import.meta.env.STRAPI_URL + resp.data?.attributes?.url;
+  else return "/no-image.jpg";
+};
+
+const articleToCard = (article: ComponentGeneralArticle, link: string) => {
+  return {
+    title: article.title,
+    description: article.description,
+    imageUrl: imageRespToUrl(article.cover),
+    link: link,
+    publishDate: article.publishDate,
+  } as CardBase;
+};
+
+export { imageRespToUrl, articleToCard };
