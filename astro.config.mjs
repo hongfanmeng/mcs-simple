6e4321107c00b4e0806970190bc1c633662f1cbb
StevenH <hongfanmeng@gmail.com>
2023-01-31 15:46:10 +0800

    chore: remove @astrojs/image and update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
@@ -11,12 +10,5 @@ import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    astroImageTools,
-    prefetch(),
-  ],
+  integrations: [tailwind(), astroImageTools, prefetch()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="@astrojs/image/client" />
+/// <reference types="astro/client" />
 
 interface ImportMetaEnv {
   readonly STRAPI_URL: string;
